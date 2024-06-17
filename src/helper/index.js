d886ea6ca7ac1fe109c41b130ffe03107ea03286
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2024-06-17 17:53:37 +0300

    added helper functions

---
+++ src/helper/index.js
@@ -0,0 +1,22 @@
+export const getRandomColor = () => {
+  const randomNumber = Math.floor(Math.random() * 16777215);
+  return `#${randomNumber.toString(16).padStart(6, "0")}`;
+};
+
+export const getRandomNumber = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+export const factory = (title = "", x = 0, y = 0, fill = "") => {
+  return {
+    id: Date.now(),
+    x,
+    y,
+    fill,
+    title,
+    value: "",
+    lines: [],
+  };
+};
